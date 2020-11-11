aa022ea23f7f034aaf13df4f343d244336fe4662
Dariusz Korsak <d.korsak@codeninjas.pl>
2020-11-11 17:52:19 +0100

    client ip middleware

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Middlewares\ClientIp;
 use GuzzleHttp\Psr7\Response;
 use Tuupola\Middleware\CorsMiddleware;
 use Cordo\Gateway\Core\UI\Http\Dispatcher;
@@ -16,6 +17,7 @@ $container = require __DIR__ . '/../bootstrap/app.php';
 // router
 $router = $container->get('router');
 $router->addMiddleware(new CorsMiddleware($container->get('config')->get('cors')));
+$router->addMiddleware(new ClientIp());
 $router->addMiddleware(new ApiKeyMiddleware());
 $router->addMiddleware((new RateLimitMiddlewareFactory())($container));
 $router->addMiddleware(new ParsePutRequest());
