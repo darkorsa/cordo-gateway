e6c00ddd10f08205651bd950c88f9ba8b4c87e2b
Dariusz Korsak <d.korsak@codeninjas.pl>
2021-03-07 00:08:33 +0100

    logger in DI container

--- bootstrap/definitions.php
+++ bootstrap/definitions.php
@@ -1,7 +1,10 @@
 <?php
 
+use Monolog\Logger;
 use Noodlehaus\Config;
+use Psr\Log\LoggerInterface;
 use GuzzleHttp\Psr7\Response;
+use Monolog\Handler\StreamHandler;
 use Psr\SimpleCache\CacheInterface;
 use Cordo\Gateway\Core\UI\Http\Router;
 use Psr\Http\Message\ResponseInterface;
@@ -19,12 +22,16 @@ return [
     }),
     'request' => DI\get(ServerRequestInterface::class),
     'router' => DI\get(Router::class),
+    'logger' => DI\factory(static function () {
+        return (new Logger('debug'))->pushHandler(new StreamHandler(storage_path('logs/debug.log'), Logger::DEBUG));
+    }),
     ServerRequestInterface::class => DI\factory('GuzzleHttp\Psr7\ServerRequest::fromGlobals'),
     ErrorReporterInterface::class => DI\get('error_reporter'),
     Config::class => DI\get('config'),
     CacheInterface::class => DI\factory(static function () {
         return CachePoolFactory::create('redis');
     }),
+    LoggerInterface::class => DI\get('logger'),
     ProblemDetailsResponseFactory::class => DI\factory(static function () {
         $callable = function (): ResponseInterface {
             return new Response();
