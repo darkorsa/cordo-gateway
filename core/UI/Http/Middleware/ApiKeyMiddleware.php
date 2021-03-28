cd8969415d26f04e2b628d27b89f412b894e14d3
Dariusz Korsak <d.korsak@codeninjas.pl>
2021-03-28 12:29:35 +0200

    fixes after static analysis

--- core/UI/Http/Middleware/ApiKeyMiddleware.php
+++ core/UI/Http/Middleware/ApiKeyMiddleware.php
@@ -13,9 +13,9 @@ class ApiKeyMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $xApiKey = current($request->getHeader('X-Api-Key'));
-        
-        if (!in_array($xApiKey, [env('API_KEY') , env('DEV_API_KEY')])) {
-            return new Response('401', [], 'Invalid Api Key');
+
+        if (!in_array($xApiKey, [env('API_KEY'), env('DEV_API_KEY')])) {
+            return new Response(401, [], 'Invalid Api Key');
         }
 
         // disable cache for dev env
